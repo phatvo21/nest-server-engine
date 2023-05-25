07be33650fac096bd12677c9a49a7373dca3d282
Phat Vo <phat@briohr.com>
2023-05-25 13:56:33 +0700

    Update database path

--- src/modules/base-database.module.ts
+++ src/modules/base-database.module.ts
@@ -1,25 +1,33 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import path from 'path';
 
-export type DBType = 'postgres' | 'mysql';
+const entityPath: string = path.join(__dirname, 'src/db/entities/*.entity.js');
+const migrationPath: string = path.join(__dirname, 'src/db/migrations/*.js');
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
+        type: configService.get('app.databaseType'),
         host: configService.get('app.databaseHost'),
         port: configService.get('app.databasePort'),
         username: configService.get('app.databaseUser'),
         password: configService.get('app.databasePassword'),
         database: configService.get('app.databaseName'),
-        entities: [],
-        synchronize: true,
+        entities: [entityPath],
+        migrations: [migrationPath],
+        synchronize: false,
+        autoLoadEntities: true,
+        cli: {
+          entitiesDir: `src/db/entities`,
+          migrationsDir: `src/db/migrations`,
+        },
       }),
       inject: [ConfigService],
-    }),
+    } as any),
   ],
 })
 export class BaseDatabaseModule {}
